753522b1ee03033c3582ae1c0ca28c32a1767668
LubinLew <lgbxyz@gmail.com>
2023-04-13 03:49:57 +0000

    haproxy 2.7.6

---
+++ output/BUILD/haproxy-2.7.6/include/haproxy/qmux_http.h
@@ -0,0 +1,19 @@
+#ifndef _HAPROXY_MUX_QUIC_HTTP_H
+#define _HAPROXY_MUX_QUIC_HTTP_H
+
+#ifdef USE_QUIC
+
+#include <haproxy/buf.h>
+#include <haproxy/mux_quic.h>
+
+size_t qcs_http_rcv_buf(struct qcs *qcs, struct buffer *buf, size_t count,
+                        char *fin);
+size_t qcs_http_snd_buf(struct qcs *qcs, struct buffer *buf, size_t count,
+                        char *fin);
+size_t qcs_http_reset_buf(struct qcs *qcs, struct buffer *buf, size_t count);
+
+void qcs_http_handle_standalone_fin(struct qcs *qcs);
+
+#endif /* USE_QUIC */
+
+#endif /* _HAPROXY_MUX_QUIC_HTTP_H */
